53234bcf0ba37bd95a6351e62ff178338dddb1c7
Ifatmetu <ifatjahan305@gmail.com>
2025-07-30 20:34:51 +0600

    Update Beautiful_Matrix.cpp

--- Beautiful_Matrix.cpp
+++ Beautiful_Matrix.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[5][5];
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            if(a[i][j]==1){
+
+                cout<<abs(3-i-1)+abs(3-j-1)<<endl;
+                return 0;
+            }
+        }
+    }
+
+}
 
